f14ca1f431542d135deae494324612fb93c3b2b7
koparasy <parasyris1@llnl.gov>
2021-01-26 18:27:09 -0800

    Fixed Error Message

--- approx/runtime/src/approx_profile_papi.cpp
+++ approx/runtime/src/approx_profile_papi.cpp
@@ -1,139 +1,139 @@
 #include <approx_profile.h>
-#include <iostream>
-#include <cstring>
 #include <cstdlib>
-#include <string>
-#include <papi.h>
+#include <cstring>
 #include <hdf5.h>
+#include <iostream>
+#include <papi.h>
+#include <string>
 
 #include <approx_profile_papi.h>
 
 #include "H5Gpublic.h"
 #include "io_helpers.h"
 
-
 #define PAPI_MAX_STRING_NAME 200
 #define DELIM ";"
 
-PapiProfiler::PapiProfiler(const char *FName){
-    FileId = openHDF5File(FName);
-    ProfileEvents = PAPI_NULL;
-    int ErrorCode = PAPI_library_init(PAPI_VER_CURRENT);
-    if ( ErrorCode == PAPI_VER_CURRENT ){
-        fprintf(stderr, "Could Not Initialize PAPI Library");
-        exit(-1);
+PapiProfiler::PapiProfiler(const char *FName) {
+  FileId = openHDF5File(FName);
+  ProfileEvents = PAPI_NULL;
+  int ErrorCode = PAPI_library_init(PAPI_VER_CURRENT);
+  if (ErrorCode == PAPI_VER_CURRENT) {
+    fprintf(stderr, "Could not Initialize Library\n %s \n Exiting...\n",
+            PAPI_strerror(ErrorCode));
+    exit(-1);
+  }
+
+  /* Currently nested profiling is not supported.           */
+  /* Profiling will start and end when the code enters and  */
+  /* exits a  code region respectively. So there is no need */
+  /* for an event set for each code region                  */
+  if ((ErrorCode = PAPI_create_eventset(&ProfileEvents)) != PAPI_OK) {
+    fprintf(stderr, "Could not create Event Set\n %s \n Exiting...\n",
+            PAPI_strerror(ErrorCode));
+    exit(-1);
+  }
+
+  HCounters = PAPI_num_counters();
+  const char *EPAPI = std::getenv("PROFILE_EVENTS");
+  EventNames = new char *[HCounters];
+  PapiNames = new char[strlen(EPAPI) + 1];
+  memcpy(PapiNames, EPAPI, strlen(EPAPI) + 1);
+  int i = 0;
+
+  EventNames[i] = std::strtok(PapiNames, DELIM);
+  LEvents = 0;
+
+  while (EventNames[i] != nullptr) {
+    EventNames[++i] = std::strtok(NULL, DELIM);
+  }
+
+  LEvents = i;
+  if (LEvents > HCounters) {
+    fprintf(stderr, "Multiplexing a.t.m is not supported\n");
+    exit(-1);
+  }
+
+  int Events[LEvents];
+  for (i = 0; i < LEvents; i++) {
+    if (PAPI_event_name_to_code(EventNames[i], &Events[i]) != PAPI_OK) {
+      fprintf(stderr, "Coulot not get event name\n exiting...\n");
+      exit(-1);
     }
+  }
 
-    /* Currently nested profiling is not supported.           */
-    /* Profiling will start and end when the code enters and  */
-    /* exits a  code region respectively. So there is no need */
-    /* for an event set for each code region                  */
-    if ((ErrorCode = PAPI_create_eventset(&ProfileEvents) ) != PAPI_OK){
-        fprintf(stderr, "Could not create Event Set\n %s \n Exiting...\n", PAPI_strerror(ErrorCode));
-        exit(-1);
-    }
+  if (PAPI_create_eventset(&ProfileEvents) != PAPI_OK) {
+    fprintf(stderr, "Could not create event set\n Exiting...\n");
+    exit(-1);
+  }
 
-    HCounters = PAPI_num_counters();
-    const char *EPAPI = std::getenv("PROFILE_EVENTS");
-    EventNames = new char*[HCounters];
-    PapiNames = new char[strlen(EPAPI)+1];
-    memcpy(PapiNames, EPAPI, strlen(EPAPI)+1);
-    int i = 0;
+  if (PAPI_add_events(ProfileEvents, Events, LEvents) != PAPI_OK) {
+    fprintf(stderr, "Could not add events\n Exiting\n");
+    exit(-1);
+  }
 
-    EventNames[i] = std::strtok(PapiNames, DELIM);
-    LEvents = 0;
-
-    while(EventNames[i] != nullptr){
-        EventNames[++i] = std::strtok(NULL, DELIM);
-    }
-
-    LEvents = i;
-    if (LEvents > HCounters) {
-        fprintf(stderr, "Multiplexing a.t.m is not supported\n");
-        exit(-1);
-    }
-
-    int Events[LEvents];
-    for ( i = 0; i < LEvents; i++){
-        if ( PAPI_event_name_to_code(EventNames[i], &Events[i]) != PAPI_OK) {
-            fprintf(stderr, "Coulot not get event name\n exiting...\n");
-            exit(-1);
-        }
-    }
-
-    if ( PAPI_create_eventset(&ProfileEvents) != PAPI_OK) {
-        fprintf(stderr, "Could not create event set\n Exiting...\n");
-        exit(-1);
-    }
-
-    if ( PAPI_add_events(ProfileEvents, Events, LEvents) != PAPI_OK){
-        fprintf(stderr, "Could not add events\n Exiting\n");
-        exit(-1);
-    }
-
-    TStats = new long long [LEvents];
+  TStats = new long long[LEvents];
 }
 
+PapiProfiler::~PapiProfiler() {
+  // I need to write all the data to the respective
+  // group in the HDF5 file.
+  for (auto region : AddrToStats) {
+    char *RName = region.second->getName();
+    long long *stats = region.second->getStats();
+    unsigned int NInvocations = region.second->getInvocations();
+    hid_t GId = createOrOpenGroup(RName, FileId);
+    hid_t GProfile = createOrOpenGroup("ProfileData", GId);
 
-
-PapiProfiler::~PapiProfiler(){
-    // I need to write all the data to the respective
-    // group in the HDF5 file.
-    for ( auto region : AddrToStats){
-        char *RName = region.second->getName();
-        long long *stats = region.second->getStats();
-        unsigned int NInvocations = region.second->getInvocations();
-        hid_t GId = createOrOpenGroup(RName, FileId);
-        hid_t GProfile = createOrOpenGroup("ProfileData", GId);
-
-        for (int i = 0; i < LEvents; i++){
-            writeProfileData(EventNames[i],GProfile, stats[i]/(double) NInvocations);
-        }
-
-        H5Gclose(GId);
-        H5Gclose(GProfile);
+    for (int i = 0; i < LEvents; i++) {
+      writeProfileData(EventNames[i], GProfile,
+                       stats[i] / (double)NInvocations);
     }
-    H5Fclose(FileId);
+
+    H5Gclose(GId);
+    H5Gclose(GProfile);
+  }
+  H5Fclose(FileId);
 }
 
-void PapiProfiler::startProfile(const char *RName, uintptr_t FnAddr){
-    if ( PAPI_start(ProfileEvents) != PAPI_OK){
-        fprintf(stderr, "Could not start counters\nExiting...\n");
-        exit(-1);
-    }
-    return;
+void PapiProfiler::startProfile(const char *RName, uintptr_t FnAddr) {
+  if (PAPI_start(ProfileEvents) != PAPI_OK) {
+    fprintf(stderr, "Could not start counters\nExiting...\n");
+    exit(-1);
+  }
+  return;
 }
 
-void PapiProfiler::stopProfile(const char *RName, uintptr_t FnAddr){
-    if ( PAPI_stop(ProfileEvents, TStats) != PAPI_OK ){
-        fprintf(stderr, "Could not stop counters\nExiting...\n");
-        exit(-1);
-    }
+void PapiProfiler::stopProfile(const char *RName, uintptr_t FnAddr) {
+  if (PAPI_stop(ProfileEvents, TStats) != PAPI_OK) {
+    fprintf(stderr, "Could not stop counters\nExiting...\n");
+    exit(-1);
+  }
 
-    if (AddrToStats.find(FnAddr) == AddrToStats.end()){
-        RegionProfiler *NRegion = new RegionProfiler(RName, LEvents);
-        AddrToStats[FnAddr] = NRegion;
-    }
-    AddrToStats[FnAddr]->increaseStats(TStats);
-    return;
+  if (AddrToStats.find(FnAddr) == AddrToStats.end()) {
+    RegionProfiler *NRegion = new RegionProfiler(RName, LEvents);
+    AddrToStats[FnAddr] = NRegion;
+  }
+  AddrToStats[FnAddr]->increaseStats(TStats);
+  return;
 }
 
 RegionProfiler::RegionProfiler(const char *Name, int NStats) : NStats(NStats) {
-    RName = new char[strlen(Name)+1];
-    std::strcpy(RName, Name);
-    Accum = new long long [NStats];
-    NInvocations = 0;
-    memset(Accum, 0, NStats * sizeof(unsigned long long));
+  RName = new char[strlen(Name) + 1];
+  std::strcpy(RName, Name);
+  Accum = new long long[NStats];
+  NInvocations = 0;
+  memset(Accum, 0, NStats * sizeof(unsigned long long));
 }
 
-RegionProfiler::~RegionProfiler(){
-    delete [] Accum;
-    delete [] RName;
+RegionProfiler::~RegionProfiler() {
+  delete[] Accum;
+  delete[] RName;
 }
 
-void RegionProfiler::increaseStats(long long *CStats){
-    NInvocations +=1;
-    for (unsigned int i = 0; i < NStats; i++){
-        Accum[i] += CStats[i];
-    }
+void RegionProfiler::increaseStats(long long *CStats) {
+  NInvocations += 1;
+  for (unsigned int i = 0; i < NStats; i++) {
+    Accum[i] += CStats[i];
+  }
 }
