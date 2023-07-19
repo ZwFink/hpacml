c406ed3206a4a1d145fcf6b5c685cbbb7f97d301
Zane Fink <finkzane@gmail.com>
2023-07-19 12:43:59 -0500

    add a 5-point stencil example

--- approx/test_parse/t_decl.cpp
+++ approx/test_parse/t_decl.cpp
@@ -9,7 +9,7 @@ int main()
 	b = a[0] + 2;
 	//#pragma approx declare tensor_functor(functor_name: [0:10] = ([::5], [0:10+c], [0:c*10:1]))
   	#pragma approx declare tensor_functor(functor_name: [i, 0:6] = ([i], [i], [i], [i], [i], [i]))
-  	{}
+  	#pragma approx declare tensor_functor(fivepoint_stencil: [i, j, 0:5] = ([i,j-1:j+2], [i-1,j], [i+1,5]))
   	#pragma approx declare tensor(first_tnesor: functor_name(a[0:N], a[0:N], a[0:N], a[0:N], a[0:N], a[0:N]))
   	{}
   	#pragma approx declare tensor_functor(functor_name: [0:c+10] = ([0:c,0:c+5:10]))
